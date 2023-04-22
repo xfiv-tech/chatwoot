b0370feefe9cc25e33c0ec5cc9fefaeb7733d625
DarioMarret <javier_dario_marret@hotmail.com>
2023-04-21 21:13:01 -0500

    up

--- up.sh
+++ up.sh
@@ -1,3 +1,4 @@
+docker login -u "intelnexoec" -p "dckr_pat_EY2hedAJR91hc9Vn9Hx6-qVpgg0"
 docker-compose run --rm rails bundle exec rails db:chatwoot_prepare
 seelp 5
 docker exec -it $(basename $(pwd))_rails_1 sh -c 'RAILS_ENV=production bundle exec rails c'
