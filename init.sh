75d36d6a546d808282310ed4f8bf167b28fa6e30
DarioMarret <javier_dario_marret@hotmail.com>
2023-04-26 17:40:41 -0500

    .env

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
-docker-compose build
+# docker-compose build
 
-docker-compose run --rm rails bundle exec rails db:chatwoot_prepare
+# docker-compose run --rm rails bundle exec rails db:chatwoot_prepare
 
-docker-compose up -d
+# docker-compose up -d
 
 
 
-# docker-compose -f docker-compose.production.yaml build
-# docker-compose -f docker-compose.production.yaml up
+docker-compose -f docker-compose.production.yaml build
+docker-compose -f docker-compose.production.yaml up -d
 # docker-compose run --rm --service-port rails
