aacbe1936f524b6011e20280ee62d4e16b09cf82
DarioMarret <javier_dario_marret@hotmail.com>
2023-05-12 14:07:43 -0500

    2023-05-12 dev_00c3f501 exchange

--- setting.sh
+++ setting.sh
@@ -2,30 +2,35 @@
 # Preguntar si desea reiniciar el back, front o un contendeor en específico
 # si elige back, reiniciar_back.sh
 # si elige front, reiniciar_front.sh
+# reconstruir imagen de contenedor
 #  escriba el nombre del contenedor que desea reiniciar de la lista presentada anterior mente
 
 
 docker ps --format "table {{.Names}}"
-echo "Configuracion de contenedores: "
-echo "1. Reiniciar back"
-echo "2. Reiniciar front"
-echo "3. Reiniciar contenedor en específico"
+echo ""
+echo ""
+echo "##############################################"
+echo "1. Reiniciar contenedor en específico"
+echo "2. Reconstruir imagen de contenedor"
+echo "0. Salir"
+echo "###############################################"
+echo ""
+echo ""
 
 read -p "Escriba el número de la opción que desea ejecutar: " opcion
 
 if [ $opcion -eq 1 ]
-then
-    echo "Reiniciando back"
-    ./reiniciar_back.sh
-elif [ $opcion -eq 2 ]
-then
-    echo "Reiniciando front"
-    ./reiniciar_front.sh
-elif [ $opcion -eq 3 ]
-then
     echo "Reiniciando contenedor en específico"
     read -p "Escriba el nombre del contenedor que desea reiniciar: " nombre_contenedor
     docker restart $nombre_contenedor
+elif [ $opcion -eq 2 ]
+    echo "Reconstruir imagen de contenedor"
+    read -p "Escriba el nombre del contenedor que desea reconstruir: " nombre_contenedor
+    docker stop $nombre_contenedor
+    docker rm $nombre_contenedor
+    docker-compose build $nombre_contenedor
+elif [ $opcion -eq 0 ]
+    echo "Listo."
 else
     echo "Opción no válida"
 fi
