8687d237262743750b6e091527017f4c1130b98f
DJMarret <javier_dario_marret@hotmail.com>
2023-05-28 21:55:26 -0500

    token

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
+docker login -u "intelnexoec" -p "dckr_pat_EY2hedAJR91hc9Vn9Hx6-qVpgg0"
 docker-compose build
-
 docker-compose run --rm rails bundle exec rails db:chatwoot_prepare
-
 docker-compose up -d
-
-docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.Size}}" > tabla_contenedores.txt
+docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.Size}}"
 
